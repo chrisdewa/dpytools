2a0ae875c8c6220d7a531795199af139c35572a6
chrisdewa <62080903+chrisdewa@users.noreply.github.com>
2021-02-03 09:38:38 -0600

    Improved owner_cog to handle cog subfolders

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+
+# 0.0.11a
+- Improved cogs command from owner_cog
+
 # 0.0.10a
 
 - Added more proper documentation.
--- dpytools/owner_cog.py
+++ dpytools/owner_cog.py
@@ -43,7 +43,6 @@ class OwnerCog(commands.Cog):
 
         Raises:
             InvalidOption if option is not recognized
-            UnknownCog if cog name is not found within the specified directory
         """
         if option.lower() not in ['load', 'unload', 'reload', 'list']:
             raise InvalidOption(f"{option} is not valid. Valid options are:'load', 'unload', 'reload' and 'list' ")
@@ -56,7 +55,13 @@ class OwnerCog(commands.Cog):
             'reload': bot.reload_extension
         }
 
-        cogs = [filename[:-3] for filename in os.listdir(cogs_dir) if filename.endswith('.py')]
+        cogs = [found_cog for dp, dn, fn in
+                os.walk(os.path.expanduser(cogs_dir)) for f in fn
+                if '__pycache__' not in (found_cog := os.path.join(dp, f)
+                                         .replace(cogs_dir, '')
+                                         .replace('\\', '.')
+                                         .replace('/', '.')[1:-3])]
+        # cogs = [filename[:-3] for filename in os.listdir(cogs_dir) if filename.endswith('.py')]
 
         if option.lower() == 'list':
             await ctx.send(embed=Embed(
@@ -68,16 +73,26 @@ class OwnerCog(commands.Cog):
             action = actions[option]
 
             if cog.lower() == 'all':
-                msg = await ctx.send(f'Doing: {action.__name__} on all')
+                msg = await ctx.send(embed=Embed(description=f'Performing: {action.__name__} on all extensions'))
                 for cog in cogs:
                     try:
                         action(f"{cogs_dir[2:]}.{cog}")
-                    except ExtensionError:
-                        pass
+                    except ExtensionError as E:
+                        await msg.edit(embed=Embed(description=f"Action could not be completed.\n"
+                                                               f"Reason:\n"
+                                                               f"```{E}```"))
+                    else:
+                        await msg.edit(embed=Embed(description=f'**Done!**'))
             elif cog not in cogs:
-                raise UnknownCog(f"{cog} is not a valid cog, check spelling and verify directory.")
+                await ctx.send(embed=Embed(
+                    description=f"{cog} is not a valid cog, check spelling and verify directory."))
             else:
-                msg = await ctx.send(f'Doing: {action.__name__} on {cog}')
-                action(f"{cogs_dir[2:]}.{cog}")
-
-            await msg.edit(content=f'**Done!**')
+                msg = await ctx.send(embed=Embed(description=f'Performing: {action.__name__} on {cog}'))
+                try:
+                    action(f"{cogs_dir[2:]}.{cog}")
+                except ExtensionError as E:
+                    await msg.edit(embed=Embed(description=f"Action could not be completed.\n"
+                                                           f"Reason:\n"
+                                                           f"```{E}```"))
+                else:
+                    await msg.edit(embed=Embed(description=f'**Done!**'))
