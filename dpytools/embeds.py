d04f25919e4974bcce4fad8e04ec9a7e49211f5d
Kshitiz Arya <aryakshitiz17@gmail.com>
2021-05-30 13:05:13 +0530

    Added a new class EmbedList
    It takes a dictionary containing name and values as key value pair and split them into several embeds according to size provided by the user.

--- dpytools/embeds.py
+++ dpytools/embeds.py
@@ -8,6 +8,7 @@ from typing import List, Optional, Union, Dict
 import discord
 from discord import Embed
 from discord.ext.commands import Paginator
+from datetime import datetime
 
 
 def paginate_to_embeds(description: str,
@@ -118,3 +119,141 @@ class Embed(discord.Embed):
         """
         for name, value in kwargs.items():
             self.add_field(name=name, value=value, inline=inline)
+
+            
+class EmbedList:
+    
+    """A class that takes dictionary containing name and value and splint them into several Embeds.
+
+    Attributes
+    -----------
+    dicts: Optional[:class:`dict`]
+        The dictionary containing name and values as key-value pair. Must not be empty
+    title: Optional[:class:`str`]
+        Title of the embeds. 
+    description: :class:`int`
+        Description of the embeds.
+    footer: Optional[:class:`str`]
+        Footer of the embeds
+    author: Optional[:class:`str`]
+        Author of the embeds
+    inline: Optional[:class:`bool`]
+        Bool variable if fields will be inline or not. Defaults to True
+    size: Optional[:class:`int`]
+        Number of field in each embed. Must be greater then 0. Defaults to 25
+    color: Optional[:class:`discord.Color`, :class: `int`]
+        The color of the disord embed. Default is Black
+    """
+
+    def __init__(self, **options):
+        self.dicts = options.pop('dicts')
+        self.title = options.pop('title', '')
+        self.description = options.pop('description', '')
+        self.footer = options.pop('footer', '')
+        self.author = options.pop('author', '')
+        self.inline = options.pop('inline', True)
+        self.colour = options.pop('colour', 0)
+        self.size = options.pop('size', 25)
+        
+        self.field_limit = 25
+        self.char_limit = 6000
+        self.clear()
+        self._add_embed(self.dicts)
+
+    def clear(self):
+        """Clears the paginator to have no pages."""
+        self._pages = []
+
+    def _check_embed(self, embed: discord.Embed, *chars: str):
+        """
+        Check if the emebed is too big to be sent on discord
+
+        Args:
+            embed (discord.Embed): The embed to check
+
+        Returns:
+            bool: Will return True if the emebed isn't too large
+        """
+        check = (
+            len(embed) + sum(len(char) for char in chars if char) < self.char_limit
+            and len(embed.fields) < self.field_limit
+        )
+        return check
+
+    def _new_page(self):
+        """
+        Create a new page
+
+        Args:
+            title (str): The title of the new page
+
+        Returns:
+            discord.Emebed: Returns an embed with the title and color set
+        """
+        return discord.Embed(title=self.title, description=self.description, timestamp=datetime.now(), color=self.colour)
+
+    def _add_page(self, page: discord.Embed):
+        """
+        Add a page to the paginator
+
+        Args:
+            page (discord.Embed): The page to add
+        """
+        page.set_footer(text=self.footer)
+        page.set_author(name=self.author)
+        self._pages.append(page)
+
+    def _chunks(self, dicts:dict):
+        """Yield successive chunks of num size from dicts
+
+        Args:
+            dicts (dict): Dictionary containing name and value
+
+        Yields:
+            tuple_list (list): list chunks of size num containing name and value
+        """        
+
+        num = self.size
+        if not dicts:
+            raise ValueError("Dictionary containing name and value can't be empty!")
+
+        tuple_list = [(name, value) for name, value in dicts.items()]
+
+        if num < 1:
+            raise ValueError("Number of Embed fields can't be zero")
+
+        for i in range(0, len(tuple_list), num):
+            yield tuple_list[i:i+num]
+    
+    def _add_embed(self, dicts):
+        """Add embeds to pages
+
+        Args:
+            dicts :- Name, Value dictionary to generate embed from
+
+        Returns:
+            None
+
+        """        
+        for d in self._chunks(dicts):
+            embed = self._new_page()
+
+            for tup in d:
+                name, value = tup
+                embed.add_field(name=name, value=value, inline=self.inline)
+
+            self._add_page(embed)
+
+    @property
+    def pages(self):
+        """Returns the rendered list of pages."""
+        if len(self._pages) == 1:
+            return self._pages
+        lst = []
+        for page_no, page in enumerate(self._pages, start=1):
+            page: discord.Embed
+            page.description = (
+                f"`Page: {page_no}/{len(self._pages)}`\n{page.description}"
+            )
+            lst.append(page)
+        return lst
